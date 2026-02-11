fcb35627d3a5c7a0f37b929986e441cb0767c895
dr-pudding <jackmj@proton.me>
2026-02-11 06:05:39 -0600

    complete nixvim implementation!

--- home/shell/flake.nix
+++ home/shell/flake.nix
@@ -4,6 +4,8 @@
             url = "github:nix-community/nixvim/nixos-25.11";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+
+        nixpkgs.follows = "nixpkgs";
     };
 
     outputs = {nixvim, ...}: {
