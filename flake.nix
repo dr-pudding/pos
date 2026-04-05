2ca0f8e9eb517e550b5bd1357fb65d1aba78af4d
dr-pudding <jackmj@proton.me>
2026-04-04 19:57:57 -0500

    added authentik flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,18 @@
 {
     description = "puddingOS - A high-level configuration layer for NixOS and Home Manager.";
 
-    outputs = {self}: {
+    inputs = {
+        authentik-nix.url = "github:nix-community/authentik-nix";
+    };
+
+    outputs = {
+        self,
+        authentik-nix,
+    }: {
         nixosModules.default = import ./modules/nixos;
+        nixosModules.server = {pkgs, ...}: {
+            imports = [authentik-nix.nixosModules.default];
+        };
         homeManagerModules.default = import ./modules/home-manager;
     };
 }
