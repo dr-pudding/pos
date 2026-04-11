449f48cb8ec35c8624beeb4b8cb20de38026dc18
dr-pudding <jackmj@proton.me>
2026-04-11 10:53:23 -0500

    pos cli tool changes

--- modules/nixos/cmd/pos.py
+++ modules/nixos/cmd/pos.py
@@ -1,5 +1,4 @@
 import click
-from pos_secrets import secrets
 from static import run_cmd
 
 from os import system as os_system
@@ -13,27 +12,41 @@ def cmd(ctx):
         click.echo(ctx.get_help())
 
 
-cmd.add_command(secrets)
-
-
 @cmd.command()
+@click.option(
+    "--flake-hostname",
+    default="",
+    show_default=False,
+    help="Specify a non-matching hostname to build for when rebuilding with flakes.",
+)
 @click.option(
     "--offline",
     help="Disable network features to ensure successful offline build.",
     is_flag=True,
 )
-def rebuild(offline: bool = False):
+def rebuild(flake_hostname: str = "", offline: bool = False):
     """Rebuild the system."""
+    cmd = "sudo nixos-rebuild switch"
+
+    if flake_hostname != "":
+        cmd += f" --flake /etc/nixos/flake.nix#{flake_hostname}"
     if offline:
-        run_cmd("sudo nixos-rebuild switch --offline")
-    else:
-        run_cmd("sudo nixos-rebuild switch")
+        cmd += " --offline"
+
+    run_cmd(cmd)
 
 
 @cmd.command()
 def update():
     """Update flake inputs."""
     run_cmd("sudo nix flake update --flake /etc/nixos")
+
+
+@cmd.command()
+def upgrade():
+    """Upgrade the system (update + rebuild)."""
+    update.invoke(ctx=click.Context(update))
+    rebuild.invoke(ctx=click.Context(rebuild))
 
 
 @cmd.command()
